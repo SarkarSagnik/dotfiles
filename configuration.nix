79910ccd43335fad2aebeae8774a177bd55b9f98
Sagnik Sarkar <94705138+SarkarSagnik@users.noreply.github.com>
2026-01-26 13:25:58 +0530

    Configure GDM, add services and system packages
    Updated display manager to use GDM and added various services and packages including audio, Bluetooth, and Flatpak support.

--- configuration.nix
+++ configuration.nix
@@ -14,16 +14,22 @@
 
   time.timeZone = "Asia/Kolkata";
 
-  services.displayManager.ly.enable = true;
+  services.displayManager.gdm.enable = true;
   services.xserver = {
     enable = true;
     autoRepeatDelay = 200;
     autoRepeatInterval = 35;
+    displayManager.gdm.wayland = true;
+    windowManager.niri = {
+      enable = true;
+      package = pkgs.niri;
+    };
   };
 
   users.users.yhwach = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "audio" "video" "input" "dialout" "flatpak" ];
     packages = with pkgs; [
       tree
     ];
@@ -34,18 +40,71 @@
   environment.systemPackages = with pkgs; [
     neovim
     wget
-    alacritty
     git
     ghostty
     brave
     emacs
+    niri
+    noctalia-shell
+    firefox
+    vlc
+    gimp
+    inkscape
+    libreoffice
+    discord
+    spotify
+    pavucontrol
+    blueman
+    networkmanagerapplet
+    file-roller
+    gnome-disk-utility
+    gnome-system-monitor
   ];
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
+    (nerd-fonts.override { fonts = [ "JetBrainsMono" "FiraCode" ]; })
   ];
 
+  # Audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # Printing
+  services.printing.enable = true;
+
+  # Enable Flatpak
+  services.flatpak.enable = true;
+  
+  # Add Flathub repository
+  systemd.tmpfiles.rules = [
+    "d /var/lib/flatpak 0755 root root -"
+  ];
+  
+  # Configure Flatpak remotes
+  system.activationScripts.flatpak = ''
+    ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  '';
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # NetworkManager applet
+  programs.nm-applet.enable = true;
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "25.11";
+  system.stateVersion = "25.05";
 
 }  
